be4e76f3d0e730572de52431d64386fc1ecb5e8e
周振宇 <zhouzhenyu@xforceplus.com>
2022-08-01 18:26:01 +0800

    init

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from './index.less';
+import '@ant-design/compatible/assets/index.less'
+export default function IndexPage() {
+  return (
+    <div>
+      <h1 className={styles.title}>Page index</h1>
+    </div>
+  );
+}
